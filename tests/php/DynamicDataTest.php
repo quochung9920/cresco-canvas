db53f871e5de7bd2c66c1490c06851b14be301c5
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 01:10:45 +0700

    test: cover Dynamic Data phase two normalization

--- tests/php/DynamicDataTest.php
+++ tests/php/DynamicDataTest.php
@@ -15,6 +15,7 @@ final class DynamicDataTest extends TestCase {
 				'postType'     => 'not-public',
 				'postsPerPage' => 999,
 				'offset'       => 999,
+				'paged'        => 9999,
 				'order'        => 'sideways',
 				'orderby'      => 'meta_value',
 			)
@@ -22,7 +23,8 @@ final class DynamicDataTest extends TestCase {
 
 		self::assertSame( 'post', $args['post_type'] );
 		self::assertSame( 24, $args['posts_per_page'] );
-		self::assertSame( 200, $args['offset'] );
+		self::assertSame( 2000, $args['offset'] );
+		self::assertSame( 999, $args['paged'] );
 		self::assertSame( 'DESC', $args['order'] );
 		self::assertSame( 'date', $args['orderby'] );
 		self::assertSame( 'publish', $args['post_status'] );
@@ -34,6 +36,7 @@ final class DynamicDataTest extends TestCase {
 				'postType'     => 'page',
 				'postsPerPage' => 4,
 				'offset'       => 2,
+				'paged'        => 3,
 				'order'        => 'ASC',
 				'orderby'      => 'title',
 			)
@@ -41,8 +44,40 @@ final class DynamicDataTest extends TestCase {
 
 		self::assertSame( 'page', $args['post_type'] );
 		self::assertSame( 4, $args['posts_per_page'] );
-		self::assertSame( 2, $args['offset'] );
+		self::assertSame( 10, $args['offset'] );
+		self::assertSame( 3, $args['paged'] );
 		self::assertSame( 'ASC', $args['order'] );
 		self::assertSame( 'title', $args['orderby'] );
+	}
+
+	/**
+	 * @dataProvider preset_provider
+	 */
+	public function test_query_presets_are_deterministic( string $preset, string $orderby, string $order ): void {
+		$args = DynamicData::sanitize_query( array( 'preset' => $preset ) );
+		self::assertSame( $orderby, $args['orderby'] );
+		self::assertSame( $order, $args['order'] );
+	}
+
+	public static function preset_provider(): array {
+		return array(
+			'recent'       => array( 'recent', 'date', 'DESC' ),
+			'oldest'       => array( 'oldest', 'date', 'ASC' ),
+			'alphabetical' => array( 'alphabetical', 'title', 'ASC' ),
+			'random'       => array( 'random', 'rand', 'DESC' ),
+		);
+	}
+
+	public function test_page_parameter_is_sanitized_and_bounded(): void {
+		self::assertSame( 'loop_page', DynamicData::sanitize_page_param( 'Loop Page!' ) );
+		self::assertSame( 'cc_page', DynamicData::sanitize_page_param( '' ) );
+		self::assertSame( 32, strlen( DynamicData::sanitize_page_param( str_repeat( 'a', 80 ) ) ) );
+	}
+
+	public function test_image_resolver_returns_empty_shape_without_post(): void {
+		self::assertSame(
+			array( 'id' => 0, 'url' => '', 'alt' => '' ),
+			DynamicData::resolve_image( array( 'source' => 'featured' ), 0 )
+		);
 	}
 }
