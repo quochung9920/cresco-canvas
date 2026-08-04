76c315ff56a60ecb19ecf82602203400d9deb46b
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 00:22:39 +0700

    build: add templates runtime manifest

---
+++ build/templates.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '0.6.0-alpha.1-templates');
