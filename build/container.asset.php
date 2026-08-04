ead662a0585d7a344c411f275d3ac6875a737525
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-04 18:57:23 +0700

    build: version responsive Container runtime

--- build/container.asset.php
+++ build/container.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0.3.1-alpha.1-container');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0.4.0-alpha.1-container');
