eeca96bc7fc6eea5c90d72b88dbdf50f74502a64
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-04 17:27:35 +0700

    build: add WordPress blocks dependency

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'e86f70e0a9e51405c0d8');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'cresco-elements-20260804-1');
