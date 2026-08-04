a2c8c7d9580875d66727ba8037b4a6f590a00a19
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-04 18:51:27 +0700

    build: align responsive container dependencies

--- build/container.asset.php
+++ build/container.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd737b1bb050dab3d420');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0.3.1-alpha.1-container');
