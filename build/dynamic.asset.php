73f87376d4d90e9750a2fce24b6696842fb2ac5b
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 01:09:57 +0700

    build: advance Dynamic Data runtime to alpha 2

--- build/dynamic.asset.php
+++ build/dynamic.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch','wp-block-editor','wp-blocks','wp-components','wp-element','wp-i18n'), 'version' => '0.8.0-alpha.1-dynamic');
+<?php return array('dependencies' => array('wp-api-fetch','wp-block-editor','wp-blocks','wp-components','wp-element','wp-i18n'), 'version' => '0.8.0-alpha.2-dynamic');
