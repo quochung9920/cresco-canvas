5a919d8590adfd313e281445024f8eeb12b81849
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 18:09:36 +0700

    build: refresh Cresco canvas stage runtime

--- build/preview.asset.php
+++ build/preview.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '0.4.0-alpha.1-preview');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-canvas-stage.1');
