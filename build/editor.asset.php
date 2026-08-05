88753308a34eaa43ac3aa6c4052eea923466eb0d
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 15:31:15 +0700

    build: refresh Cresco Elements editor assets

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'e6cd3eb714cbcfbc939c');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-elements-ui.1');
