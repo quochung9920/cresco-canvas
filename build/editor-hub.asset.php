5a475c9ed1dccdf47276166f2f1301e1c42389d5
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 15:04:25 +0700

    build: add unified editor hub manifest

---
+++ build/editor-hub.asset.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+	'dependencies' => array( 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins' ),
+	'version'      => '1.0.0-rc.1',
+);
