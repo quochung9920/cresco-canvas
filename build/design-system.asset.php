7feea4aafd03c7cc56c46dc826492a834e7b5514
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 17:22:56 +0700

    build: refresh complete blueprint exporter

--- build/design-system.asset.php
+++ build/design-system.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-global-fluid.1');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-blueprint.1');
