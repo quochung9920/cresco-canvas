31a7f3cfb9d5e403e402b52c454efccf89c74805
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 16:03:57 +0700

    feat: expose fluid global settings through REST

--- includes/API/RestApi.php
+++ includes/API/RestApi.php
@@ -18,35 +18,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class RestApi {
-	public function register() {
-		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
-	}
+	public function register() { add_action( 'rest_api_init', array( $this, 'register_routes' ) ); }
 
 	public function register_routes() {
-		register_rest_route(
-			'cresco-canvas/v1',
-			'/settings',
-			array(
-				array( 'methods' => WP_REST_Server::READABLE, 'callback' => array( $this, 'get_settings' ), 'permission_callback' => array( $this, 'can_manage_settings' ) ),
-				array( 'methods' => WP_REST_Server::EDITABLE, 'callback' => array( $this, 'save_settings' ), 'permission_callback' => array( $this, 'can_manage_settings' ) ),
-				'schema' => array( $this, 'settings_schema' ),
-			)
-		);
+		register_rest_route( 'cresco-canvas/v1', '/settings', array(
+			array( 'methods' => WP_REST_Server::READABLE, 'callback' => array( $this, 'get_settings' ), 'permission_callback' => array( $this, 'can_manage_settings' ) ),
+			array( 'methods' => WP_REST_Server::EDITABLE, 'callback' => array( $this, 'save_settings' ), 'permission_callback' => array( $this, 'can_manage_settings' ) ),
+			'schema' => array( $this, 'settings_schema' ),
+		) );
 		register_rest_route( 'cresco-canvas/v1', '/settings/reset', array( 'methods' => WP_REST_Server::CREATABLE, 'callback' => array( $this, 'reset_settings' ), 'permission_callback' => array( $this, 'can_manage_settings' ) ) );
 		register_rest_route( 'cresco-canvas/v1', '/design-tokens', array( 'methods' => WP_REST_Server::READABLE, 'callback' => array( $this, 'get_design_tokens' ), 'permission_callback' => array( $this, 'can_manage_settings' ) ) );
 	}
 
-	public function can_manage_settings() {
-		return current_user_can( 'edit_theme_options' );
-	}
-
-	public function get_settings() {
-		return new WP_REST_Response( GlobalStyles::get_settings() );
-	}
-
-	public function get_design_tokens() {
-		return new WP_REST_Response( DesignTokens::catalog( GlobalStyles::get_settings() ) );
-	}
+	public function can_manage_settings() { return current_user_can( 'edit_theme_options' ); }
+	public function get_settings() { return new WP_REST_Response( GlobalStyles::get_settings() ); }
+	public function get_design_tokens() { return new WP_REST_Response( DesignTokens::catalog( GlobalStyles::get_settings() ) ); }
 
 	public function save_settings( WP_REST_Request $request ) {
 		$settings = GlobalStyles::sanitize_settings( (array) $request->get_json_params() );
@@ -61,25 +47,28 @@ final class RestApi {
 	}
 
 	public function settings_schema() {
-		$map = array( 'type' => 'object', 'additionalProperties' => array( 'type' => 'string' ) );
+		$string_map = array( 'type' => 'object', 'additionalProperties' => array( 'type' => 'string' ) );
+		$integer_map = array( 'type' => 'object', 'additionalProperties' => array( 'type' => 'integer' ) );
 		return array(
-			'$schema'              => 'http://json-schema.org/draft-04/schema#',
+			'$schema' => 'http://json-schema.org/draft-04/schema#',
 			'additionalProperties' => false,
-			'title'                => 'cresco-canvas-settings',
-			'type'                 => 'object',
-			'properties'           => array(
-				'background'            => array( 'type' => 'string' ),
-				'containerMax'          => array( 'type' => 'integer' ),
-				'contentMax'            => array( 'type' => 'integer' ),
-				'fontFamily'            => array( 'type' => 'string' ),
-				'muted'                 => array( 'type' => 'string' ),
-				'primary'               => array( 'type' => 'string' ),
-				'radius'                => array( 'type' => 'integer' ),
-				'customColors'          => $map,
-				'aliases'               => $map,
+			'title' => 'cresco-canvas-settings',
+			'type' => 'object',
+			'properties' => array(
+				'background' => array( 'type' => 'string' ),
+				'containerMax' => array( 'type' => 'integer' ),
+				'contentMax' => array( 'type' => 'integer' ),
+				'fontFamily' => array( 'type' => 'string' ),
+				'muted' => array( 'type' => 'string' ),
+				'primary' => array( 'type' => 'string' ),
+				'radius' => array( 'type' => 'integer' ),
+				'fluidTokens' => $string_map,
+				'breakpoints' => $integer_map,
+				'customColors' => $string_map,
+				'aliases' => $string_map,
 				'removeDataOnUninstall' => array( 'type' => 'boolean' ),
-				'schemaVersion'         => array( 'type' => 'integer' ),
-				'text'                  => array( 'type' => 'string' ),
+				'schemaVersion' => array( 'type' => 'integer' ),
+				'text' => array( 'type' => 'string' ),
 			),
 		);
 	}
