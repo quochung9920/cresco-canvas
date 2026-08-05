f652fd5db964d18cabe702adcb9224618f378b6f
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 14:54:45 +0700

    build: add final form editor manifest

---
+++ build/forms-completion-editor.asset.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+	'dependencies' => array( 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n' ),
+	'version'      => '0.9.0-rc.1-forms-completion-editor',
+);
