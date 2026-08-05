8775dfeb63f17a035c1ebd3496044ebc19ee6c3b
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 18:20:09 +0700

    build: refresh global responsive range controls

--- build/design-system.asset.php
+++ build/design-system.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-blueprint.1');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-blueprint.2');
