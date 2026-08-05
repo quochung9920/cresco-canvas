3b0589b7cd8677f17c577e4d2fd095aecca8934d
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 16:32:12 +0700

    build: refresh Widget Inspector style engine runtime

--- build/widget-inspector.asset.php
+++ build/widget-inspector.asset.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 	'dependencies' => array( 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins' ),
-	'version'      => '1.0.0-rc.1-widget-inspector.1',
+	'version'      => '1.0.0-rc.1-widget-inspector.2',
 );
