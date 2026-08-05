ddd94b8c111ae11793a2f11f0b5443b790022185
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-05 18:30:19 +0700

    build: refresh resizable Elements sidebar styles

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-elements-ui.3');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-elements-ui.4');
