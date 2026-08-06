9900733c9413097963f79600c3efea565c025353
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 16:52:16 +0700

    build: load app shell after registered editor views

--- build/editor-app-shell.asset.php
+++ build/editor-app-shell.asset.php
@@ -1,5 +1,13 @@
 <?php
 return array(
-	'dependencies' => array( 'wp-components', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => '1.0.0-rc.1-editor-app-shell.1',
+	'dependencies' => array(
+		'cresco-canvas-editor',
+		'cresco-canvas-editor-foundation',
+		'cresco-canvas-widget-inspector-persistent',
+		'wp-components',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0-rc.1-editor-app-shell.2',
 );
