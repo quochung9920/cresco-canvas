b00d93fcca27f34aabfaabda2459f1b445aff0f9
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 15:10:36 +0700

    build: add rich text dependency to persistent inspector

--- build/widget-inspector-persistent.asset.php
+++ build/widget-inspector-persistent.asset.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-	'dependencies' => array( 'wp-blocks', 'wp-components', 'wp-data', 'wp-element' ),
-	'version'      => '1.0.0-rc.1-widget-inspector-persistent.1',
+	'dependencies' => array( 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-rich-text' ),
+	'version'      => '1.0.0-rc.1-widget-inspector-persistent.2',
 );
