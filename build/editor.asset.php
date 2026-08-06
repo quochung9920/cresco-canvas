f39ef11ba8adf8dc6857065038f2e0f386009e4f
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 16:50:48 +0700

    build: route editor views through foundation

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,13 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-elements-ui.4');
+<?php
+return array(
+	'dependencies' => array(
+		'cresco-canvas-editor-foundation',
+		'wp-api-fetch',
+		'wp-blocks',
+		'wp-components',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0-rc.1-elements-ui.5',
+);
