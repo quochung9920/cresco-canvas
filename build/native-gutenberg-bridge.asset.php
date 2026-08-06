9b6789bdb0940ba818d0743c6c86539a7bea4b56
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 17:13:36 +0700

    build: refresh native bridge schema cache

--- build/native-gutenberg-bridge.asset.php
+++ build/native-gutenberg-bridge.asset.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 	'dependencies' => array( 'wp-blocks', 'wp-compose', 'wp-element', 'wp-hooks' ),
-	'version'      => '1.0.0-rc.1-native-bridge.4',
+	'version'      => '1.0.0-rc.1-native-bridge.5',
 );
