3afe69157b5d5499334697cd7f42b21f9cefd5a6
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 16:53:05 +0700

    build: load Structure through unified editor foundation

--- build/structure-navigator.asset.php
+++ build/structure-navigator.asset.php
@@ -1,5 +1,12 @@
 <?php
 return array(
-	'dependencies' => array( 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => '1.0.0-rc.1-structure-navigator.2',
+	'dependencies' => array(
+		'cresco-canvas-editor-foundation',
+		'wp-blocks',
+		'wp-components',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0-rc.1-structure-navigator.3',
 );
