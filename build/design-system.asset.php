f8178679682a13f129fb4cbe43618b6dff738b70
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 17:16:33 +0700

    build: load Global Design as an app shell view

--- build/design-system.asset.php
+++ build/design-system.asset.php
@@ -1,1 +1,13 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1.0.0-rc.1-blueprint.3');
+<?php
+return array(
+	'dependencies' => array(
+		'cresco-canvas-editor',
+		'cresco-canvas-editor-foundation',
+		'wp-api-fetch',
+		'wp-components',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0-rc.1-blueprint.4',
+);
