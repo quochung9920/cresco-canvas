bef7dcab1df4cf9ed8f23cd6d4023514ced48e0e
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 15:00:15 +0700

    build: refresh refined Structure Navigator styles

--- build/structure-navigator.asset.php
+++ build/structure-navigator.asset.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 	'dependencies' => array( 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => '1.0.0-rc.1-structure-navigator.1',
+	'version'      => '1.0.0-rc.1-structure-navigator.2',
 );
