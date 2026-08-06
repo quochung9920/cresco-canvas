8f2cdfff0e7b40374526f3f7e21b191a7e6f9342
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-06 16:52:43 +0700

    build: load visual canvas through unified foundation

--- build/visual-canvas.asset.php
+++ build/visual-canvas.asset.php
@@ -1,5 +1,11 @@
 <?php
 return array(
-	'dependencies' => array( 'wp-blocks', 'wp-data', 'wp-i18n' ),
-	'version'      => '1.0.0-rc.1-visual-canvas.2',
+	'dependencies' => array(
+		'cresco-canvas-editor-app-shell',
+		'cresco-canvas-editor-foundation',
+		'wp-blocks',
+		'wp-data',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0-rc.1-visual-canvas.3',
 );
