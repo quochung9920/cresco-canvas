9d8bdebf7c84c3ff24c4951f35d084c9cbe027ce
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-07 15:37:23 +0700

    fix: decouple app shell from optional editor views

--- build/editor-app-shell.asset.php
+++ build/editor-app-shell.asset.php
@@ -1,14 +1,11 @@
 <?php
 return array(
 	'dependencies' => array(
-		'cresco-canvas-design-system',
-		'cresco-canvas-editor',
 		'cresco-canvas-editor-foundation',
-		'cresco-canvas-widget-inspector-persistent',
 		'wp-components',
 		'wp-data',
 		'wp-element',
 		'wp-i18n',
 	),
-	'version'      => '1.0.0-rc.1-editor-app-shell.4',
+	'version'      => '1.0.0-rc.1-editor-app-shell.5',
 );
