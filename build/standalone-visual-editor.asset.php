147e16a072aef58c2f81c71d21c19e7844720517
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-07 16:20:44 +0700

    build: add standalone visual editor manifest

---
+++ build/standalone-visual-editor.asset.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	'dependencies' => array(
+		'wp-api-fetch',
+		'wp-block-editor',
+		'wp-block-library',
+		'wp-blocks',
+		'wp-components',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version'      => '1.0.0-rc.1-standalone-visual-editor.1',
+);
