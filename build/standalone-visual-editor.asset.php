b5faf2ac77f3e395639c7bb521c733777eac719e
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-07 18:23:29 +0700

    build: slim standalone editor dependencies for Cresco Session

--- build/standalone-visual-editor.asset.php
+++ build/standalone-visual-editor.asset.php
@@ -2,13 +2,9 @@
 return array(
 	'dependencies' => array(
 		'wp-api-fetch',
-		'wp-block-editor',
-		'wp-block-library',
-		'wp-blocks',
 		'wp-components',
-		'wp-data',
 		'wp-element',
 		'wp-i18n',
 	),
-	'version'      => '1.0.0-rc.1-standalone-visual-editor.4',
+	'version'      => '1.0.0-rc.1-standalone-visual-editor.5',
 );
