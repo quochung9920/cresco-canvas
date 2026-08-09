d8521c835a10d3ceffa0e3e6494b9262641fe6ef
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-09 20:07:53 +0700

    chore: remove retired widget inspector manifest

--- build/widget-inspector.asset.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'dependencies' => array( 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins' ),
-	'version'      => '1.0.0-rc.1-widget-inspector.6-hotfix',
-);
