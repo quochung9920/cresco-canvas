934d163543846a0ca8e5c493e1fb8052b45b1b11
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-09 20:08:28 +0700

    chore: remove retired widget inspector compatibility manifest

--- build/widget-inspector-compat.asset.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'dependencies' => array( 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-interface', 'wp-plugins' ),
-	'version'      => '1.0.0-rc.1-widget-inspector-compat.2',
-);
