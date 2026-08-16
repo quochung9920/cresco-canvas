bffac0cfe6a3c854b044334791cad5ff0d08918b
Nguyễn Quốc Hưng <59259053+quochung9920@users.noreply.github.com>
2026-08-16 22:41:24 +0700

    feat: register professional Global Design workspace

--- cresco-canvas.php
+++ cresco-canvas.php
@@ -56,5 +56,6 @@ add_action(
 		( new CrescoCanvas\Builder\StudioStructureLayout() )->register();
 		( new CrescoCanvas\Builder\StudioUxPro() )->register();
 		( new CrescoCanvas\Builder\StudioColorHarmony() )->register();
+		( new CrescoCanvas\Builder\StudioGlobalDesignPro() )->register();
 	}
 );
